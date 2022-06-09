3e98963f2bfa536a1537c0cf1c40715d0d40c59f
Valentina Londoño Marin <valoma.vld@gmail.com>
2022-06-08 19:14:15 -0500

    Finish anecdotes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,13 +1,58 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({text, clickHandler}) =><button onClick={clickHandler}>{text}</button>
+
+const Title = ({text}) =><h2>{text}</h2>
+
+const Anecdote = ({text,votes}) => <div>{text}<p> Has {votes} votes</p></div>
+
+const Today = ({text,nextHandler,votesHandler,votes}) => {
+    return (
+        <>
+        <Title text="Anecdote of the day"/>
+        <Anecdote text={text} votes={votes}/>        
+        <Button text="Vote" clickHandler={votesHandler}/>
+        <Button text="Next anecdote" clickHandler={nextHandler}/>
+        </>
+    )
+  }
+
+  const MostVotes = ({text,votes}) => {
+    return (
+        <>
+        <Title text="Anecdote with most votes"/>
+        <Anecdote text={text} votes={votes}/>     
+        </>
+    )
+  }
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [mostVoted, setMostVoted] = useState(0)
+  const [maxVotation, setMaxVotation] = useState(0)
+  const [votes, setVotes] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 })
+
+  const nextHandler = () => {   
+        let nextRandom =  Math.floor(Math.random() * 6)
+        setSelected(nextRandom)
+  }
+
+  const votesHandler = () => {   
+        let copy = { ...votes }
+        copy[selected] += 1
+        if(copy[selected]>maxVotation){
+            setMaxVotation(copy[selected])
+            setMostVoted(selected)
+        }
+        setVotes(copy)
+    }
 
   return (
-    <div>
-      {props.anecdotes[selected]}
-    </div>
+    <>
+        <Today text={anecdotes[selected]} votes={votes[selected]} nextHandler={nextHandler} votesHandler={votesHandler}/>
+        <MostVotes text={anecdotes[mostVoted]} votes={votes[mostVoted]}/>
+    </>
   )
 }
 
