4de2426903a6762783caa80fb70156db174138b7
Valentina Londoño Marin <valoma.vld@gmail.com>
2022-07-10 21:17:43 -0500

    Fix for heroku

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -89,7 +89,7 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
